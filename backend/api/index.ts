ef47f63b315d84e86dfe4e7f2e4703ad8910212a
Zeus <steffmarcelo@gmail.com>
2023-07-16 18:39:05 -0500

    Update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,3 +1,22 @@
 import app from "../src/app";
+import UserModel from "../src/models/user";
+import env from "../src/util/validateEnv";
+const jwt = require("jsonwebtoken");
+
+app.get('/api/user', (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+        jwt.verify(token, env.JWT_SECRET, {}, async (err: any, decodedUser: { id: any; }) => {
+            if (err) throw err;
+            
+            const user = await UserModel.findById(decodedUser.id).select("+email").exec();
+            res.status(200).json(user);
+        })
+    }
+    else {
+        console.log("User token not found");
+        res.json(null);
+    }
+})
 
 export default app;
