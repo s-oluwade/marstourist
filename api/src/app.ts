226139a1fc38cb9bda1ab89b2d9ebb0bfe38763b
Zeus <steffmarcelo@gmail.com>
2023-07-20 02:42:15 -0500

    Update app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: env.CLIENT_DOMAIN,
 }));
 
 app.use(session({
