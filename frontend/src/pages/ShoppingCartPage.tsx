4af13111eb7ee34368012a945e16013931375b01
Zeus <steffmarcelo@gmail.com>
2023-08-01 01:30:57 -0500

    shopping cart page update

--- frontend/src/pages/ShoppingCartPage.tsx
+++ frontend/src/pages/ShoppingCartPage.tsx
@@ -157,20 +157,20 @@ const ShoppingCartPage = () => {
     }
 
     return (
-        <div className="container mx-auto my-auto">
+        <div className="w-full mx-auto my-auto">
 
-            <div className="flex shadow-md my-10 min-w-max max-w-7xl mx-auto">
-                <div className="w-3/4 px-10 py-10 bg-base-100 dark:bg-gray-800 rounded-l">
-                    <div className="flex justify-between border-b pb-8">
+            <div className="flex flex-col md:flex-row shadow-md my-10 min-w-max max-w-7xl mx-auto">
+                <div className="md:w-3/4 space-y-4 md:px-10 py-2 md:py-10 bg-base-100 dark:bg-gray-800 rounded-l">
+                    <div className="hidden md:flex justify-between border-b pb-8">
                         <h1 className="font-normal text-2xl">Cart</h1>
                     </div>
                     {products.length > 0 && cart && cart.products["total"] && cart.products["total"].count > 0 &&
                         <>
-                            <div className="flex mt-10 mb-5">
-                                <h3 className="font-semibold text-xs uppercase w-2/5">Product Details</h3>
-                                <h3 className="font-semibold text-xs uppercase w-1/5 text-center">Quantity</h3>
-                                <h3 className="font-semibold text-xs uppercase w-1/5 text-center">Price</h3>
-                                <h3 className="font-semibold text-xs uppercase w-1/5 text-center">Total</h3>
+                            <div className="flex mt-10 mb-5 px-4">
+                                <h3 className="font-semibold text-xs uppercase w-2/4 md:w-2/5">Product Details</h3>
+                                <h3 className="font-semibold text-xs uppercase w-1/4 md:w-1/5 text-center">Quantity</h3>
+                                <h3 className="font-semibold text-xs uppercase hidden md:w-1/5 text-center">Price</h3>
+                                <h3 className="font-semibold text-xs uppercase w-1/4 md:w-1/5 text-center">Total</h3>
                             </div>
 
                             {Object.keys(cart.products).map(function (productId, idx) {
@@ -180,12 +180,12 @@ const ShoppingCartPage = () => {
                                 const item = products.filter((product) => product._id === productId)[0];
 
                                 return (
-                                    <div key={idx} className="flex items-center bg-base-200 dark:bg-neutral -mx-8 px-6 py-5">
-                                        <div className="flex w-2/5">
-                                            <div className="w-20">
+                                    <div key={idx} className="flex items-center bg-base-200 dark:bg-neutral md:-mx-8 p-5">
+                                        <div className="flex w-2/4 md:w-2/5">
+                                            <div className="hidden md:block w-20">
                                                 <img className="h-24" src={item.images[0]} alt="" />
                                             </div>
-                                            <div className="flex flex-col justify-between ml-4 flex-grow">
+                                            <div className="flex flex-col justify-between md:ml-4 flex-grow">
                                                 <span className="font-medium text-sm">{item.title}</span>
                                                 <span className="text-secondary text-xs">{item.brand}</span>
                                                 <div>
@@ -193,7 +193,7 @@ const ShoppingCartPage = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="flex justify-center w-1/5">
+                                        <div className="flex justify-center w-1/4 md:w-1/5">
                                             <a onClick={(e) => reduceFromCart(e, productId)} className="flex cursor-pointer">
                                                 <svg className="fill-current w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                                                 </svg>
@@ -206,13 +206,13 @@ const ShoppingCartPage = () => {
                                                 </svg>
                                             </a>
                                         </div>
-                                        <span className="text-center w-1/5 font-normal text-sm">
+                                        <span className="text-center hidden md:w-1/5 font-normal text-sm">
                                             {item.price.toLocaleString("en-US", {
                                                 style: "currency",
                                                 currency: "USD",
                                             })}
                                         </span>
-                                        <span className="text-center w-1/5 font-normal text-sm">
+                                        <span className="text-center w-1/4 font-normal text-sm">
                                             {(item.price * cart.products[productId].count).toLocaleString("en-US", {
                                                 style: "currency",
                                                 currency: "USD",
@@ -224,16 +224,16 @@ const ShoppingCartPage = () => {
                         </>
                     }
 
-                    <Link to='/store' className="cursor-pointer inline-flex link font-medium text-sm mt-10">
+                    <Link to='/store' className="hidden md:inline-flex cursor-pointer link font-medium text-sm">
                         <svg className="fill-current mr-2 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
                         Continue Shopping
                     </Link>
                 </div>
 
-                <div id="summary" className="w-1/4 px-8 py-10 bg-base-200 dark:bg-gray-700 rounded-r">
-                    <div className="h-full flex flex-col">
-                        <h1 className="font-normal text-2xl border-b pb-8">Payment Details</h1>
-                        <div className="flex justify-between mt-10 mb-5">
+                <div id="summary" className="md:w-1/4 px-8 py-5 bg-base-200 dark:bg-gray-700 rounded-r">
+                    <div className="flex flex-col">
+                        <h1 className="hidden md:block font-normal text-2xl border-b pb-8">Payment Details</h1>
+                        <div className="flex justify-between md:mt-10 mb-5">
                             <span className="font-medium text-sm uppercase">Items {cart && cart.products["total"] && cart.products["total"].count}</span>
                             <span className="font-medium text-sm">{totalCost}$</span>
                         </div>
@@ -249,8 +249,8 @@ const ShoppingCartPage = () => {
                         </div>
                         <ConfirmationModal message={`Buy ${numberOfItems} items with ${totalCost > 0 ? (totalCost / 1000).toFixed(3) : 0} mrs?`} title={purchaseConfirmationTitle} />
                     </div>
-                    <div className="text-center">
-                        Credit: {user?.credit ? user.credit.toFixed(3) : 0} MARS
+                    <div className="text-center pt-2">
+                        Balance: {user?.credit ? user.credit.toFixed(3) : 0} MARS
                     </div>
                 </div>
             </div>
