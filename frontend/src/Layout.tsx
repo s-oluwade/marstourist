8dca23464943053318c885981d80715db7ab8acb
Zeus <steffmarcelo@gmail.com>
2023-08-01 19:09:05 -0500

    Update Layout.tsx

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -142,16 +142,17 @@ const Layout = () => {
                                     }
 
                                     <ul className='space-y-2 font-medium w-full flex flex-col flex-1'>
-                                        <li>
-                                            <Link to={'/forum'} className="flex items-center p-2 text-neutral rounded-lg dark:text-base-300 hover:bg-gray-100 group">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
-                                                </svg>
-                                                <span className="flex-1 ml-3 whitespace-nowrap dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">Forum</span>
-                                            </Link>
-                                        </li>
+                                        
                                         {user &&
                                             <>
+                                                <li>
+                                                    <Link to={'/profile'} className="flex items-center p-2 text-neutral rounded-lg dark:text-base-300 hover:bg-gray-100 group">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                                        </svg>
+                                                        <span className="flex-1 ml-3 whitespace-nowrap dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">Profile</span>
+                                                    </Link>
+                                                </li>
                                                 <li>
                                                     <Link to={'/cart'} className="flex items-center p-2 text-neutral rounded-lg dark:text-base-300 hover:bg-gray-100 group">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">
@@ -174,8 +175,16 @@ const Layout = () => {
                                                 </li>
                                             </>
                                         }
+                                        <li>
+                                            <Link to={'/forum'} className="flex items-center p-2 text-neutral rounded-lg dark:text-base-300 hover:bg-gray-100 group">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                                                </svg>
+                                                <span className="flex-1 ml-3 whitespace-nowrap dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">Forum</span>
+                                            </Link>
+                                        </li>
                                         {!user &&
-                                            <li>
+                                            <li className='justify-end grow'>
                                                 <Link to={'/login/user'} className="flex items-center p-2 text-neutral rounded-lg dark:text-base-300 hover:bg-gray-100 group">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:group-hover:text-gray-200 dark:group-focus:text-gray-200 dark:group-active:text-gray-200">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
